efe4d03278567054bc40007171913f3ae67d4825
ifti-khan <68855379+ifti-khan@users.noreply.github.com>
2020-12-27 18:33:51 +0000

    mod: added dark mode button to nav, styled, media query and js script, Fix: few position fixes with nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
+function darkTheme() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
 
+    let darkButton = document.getElementById("dark-btn");
+    darkButton.classList.toggle("active-nav");
+
+    if (darkButton.innerHTML === "Dark Mode<br> Off") {
+        darkButton.innerHTML = "Dark Mode<br> On";
+    } else if (darkButton.innerHTML === "Dark Mode<br> On") {
+        darkButton.innerHTML = "Dark Mode<br> Off";
+    } else {
+        darkButton.innerHTML = "Dark Mode<br> On";
+    }
+}
